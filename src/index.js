8404dbae33525c7f631bd7647dae342c6340f745
da <>
2019-09-23 22:49:31 +0300

    #implement morse decoding

--- src/index.js
+++ src/index.js
@@ -37,8 +37,24 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const MAP_DICTIONARY = {
+    '00': '',
+    '10': '.',
+    '11': '-',
+    '**********': ' '
+}
+
 function decode(expr) {
-    // write your solution here
+    const maxChars = expr.length / 10;
+    const arrayOfCharIndexes = [...Array(maxChars).keys()];
+
+    return arrayOfCharIndexes.reduce( (accumulator, currenValue) => {
+        const encodedChar = expr.substr(currenValue*10, 10);
+
+        const morseCode = encodedChar.replace(/(\*\*\*\*\*\*\*\*\*\*|00|10|11)/g, (val1) => MAP_DICTIONARY[val1]);
+
+        return accumulator + (morseCode === ' ' ? ' ' : MORSE_TABLE[morseCode]);
+    }, '');
 }
 
 module.exports = {
